bbec7031a8552fcf0ab112c99fd0f36e2d325de4
qza <kokovic.zoran@gmail.com>
2016-01-21 21:21:22 +0100

    example getting configuration key from config server

---
+++ giftly-service/src/main/java/org/koko/config/ConfigController.java
@@ -0,0 +1,21 @@
+package org.koko.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+
+    @Value("${key}")
+    private String key;
+
+    @RequestMapping("/key")
+    public String getKey() {
+        return key;
+    }
+
+}
--- giftly-service/src/test/java/org/koko/AppTests.java
+++ giftly-service/src/test/java/org/koko/AppTests.java
@@ -3,12 +3,14 @@ package org.koko;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
 @WebAppConfiguration
+@TestPropertySource(locations="classpath:bootstrap-test.properties")
 public class AppTests {
 
 	@Test
