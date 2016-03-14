9fbc3d42b161bcbdd496f139c853a136fd82642d
qza <kokovic.zoran@gmail.com>
2016-03-14 19:57:35 +0100

    Initialized auth server project

---
+++ giftly-auth-server/src/main/java/org/koko/GiftlyAuthServerApplication.java
@@ -0,0 +1,12 @@
+package org.koko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftlyAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftlyAuthServerApplication.class, args);
+	}
+}
