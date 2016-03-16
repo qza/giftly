67d4a370ee9ae9ada6b0e549a0f58f7651af6349
qza <kokovic.zoran@gmail.com>
2016-03-16 21:36:41 +0100

    using test properties in search service app tests

--- giftly-search-service/src/test/java/org/koko/AppTests.java
+++ giftly-search-service/src/test/java/org/koko/AppTests.java
@@ -2,13 +2,15 @@ package org.koko;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
-@WebIntegrationTest({"server.port=0", "spring.zipkin.enabled=false"})
+@TestPropertySource(locations = "classpath:bootstrap-test.properties")
+@IntegrationTest({"server.port=0"})
 public class AppTests {
 
 	@Test
