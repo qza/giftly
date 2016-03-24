eb33b2593d3a19ddc88465270efd1d9b005bacb9
qza <kokovic.zoran@gmail.com>
2016-03-24 07:45:43 +0100

    initialized recommendation service module

---
+++ giftly-recommendation-service/src/main/java/org/koko/GiftlyRecommendationServiceApplication.java
@@ -0,0 +1,12 @@
+package org.koko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftlyRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftlyRecommendationServiceApplication.class, args);
+	}
+}
